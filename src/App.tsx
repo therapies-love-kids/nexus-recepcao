49e51ec089d8f5319fb478f3120ba0ad71387fd9
crisper2001 <crisperohman@gmail.com>
2024-11-13 17:50:58 -0300

    Experimentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+import { Button, Card, CardBody, Input } from "@nextui-org/react";
 import "./App.css";
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 
 function App() {
     const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
-  
+
     useEffect(() => {
-      setMounted(true)
+        setMounted(true)
     }, [])
-  
-    if(!mounted) return null
-  
+
+    if (!mounted) return null
+
     return (
-        <div className="bg-background">
-            <div className=" text-center w-full h-full">
-                The current theme is: {theme}
-                <button onClick={() => setTheme('light')}>Light Mode</button>
-                <button onClick={() => setTheme('dark')}>Dark Mode</button>
-            </div>
+        <div className="h-screen flex items-center justify-center bg-primary">
+            <Card className="w-2/4 shadow-xl">
+                <CardBody className="flex flex-col gap-4">
+                    The current theme is: {theme}
+                    <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+                        {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+                    </Button>
+                    <Input type="email" label="E-mail" />
+                    <Input type="password" label="Senha" />
+                    <Button color="primary">Entrar</Button>
+                </CardBody>
+            </Card>
         </div>
     );
 }
