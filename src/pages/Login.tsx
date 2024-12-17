bbe3ddcdc8c189a37ac29c1e876cd01e737224f3
Isaque <crisperohman@gmail.com>
2024-12-17 17:57:52 -0300

    ADD: Elementos de 'Login'

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import { FaAt, FaKey } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    navigate("/dashboard");
+  };
+
   return (
-    <div>
-      <h1>Login works</h1>
+    <div className="flex justify-center items-center h-screen bg-base-300">
+      <div className="card bg-base-100 w-4/5 h-4/5">
+        <div className="card-body flex justify-center items-center gap-4">
+
+          {/* Logo */}
+
+          <img src="/svg/nexus-logo-dark.svg" alt="Nexus" className="w-36" />
+
+          {/* Formulário */}
+          
+          <form className="flex flex-col gap-4 items-center" onSubmit={handleSubmit}>
+            <label className="input input-bordered flex items-center gap-2 w-full">
+              <FaAt className="text-neutral-content" />
+              <input
+                type="email"
+                className="grow"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                // required
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 w-full">
+              <FaKey className="text-neutral-content" />
+              <input
+                type="password"
+                className="grow"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                // required
+              />
+            </label>
+            <button
+              type="submit"
+              className="btn btn-primary w-1/3"
+            >
+              Entrar
+            </button>
+          </form>
+
+          <hr />
+
+          <div>
+            <p className="text-sm text-center">
+              Não possui uma conta? <Link to="/signup" className="text-primary">Registre-se</Link>
+            </p>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
