e6991cf6905fe9b2c0c1346f9f9b09d5aeb68c0b
Isaque <crisperohman@gmail.com>
2024-12-17 17:57:18 -0300

    ADD: Tema escuro do Nexus

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,22 @@ export default {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        nexus: {
+          "primary": "#008cff",
+          "secondary": "#00b4ff",
+          "accent": "#00e400",
+          "neutral": "#0c1013",
+          "base-100": "#292929",
+          "info": "#00c0ff",
+          "success": "#1b8b00",
+          "warning": "#ff9c00",
+          "error": "#d60037",
+        },
+      },
+    ],
+  },
 }
 
