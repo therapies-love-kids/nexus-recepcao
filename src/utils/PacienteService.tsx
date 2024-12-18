13fa6ea8fa213f4fa5eb1b924c8194e5cdd7b37b
Isaque <crisperohman@gmail.com>
2024-12-18 15:43:55 -0300

    ADD: Método 'create' em 'PacienteService'

--- src/utils/PacienteService.tsx
+++ src/utils/PacienteService.tsx
@@ -4,6 +4,11 @@ const BASE_URL = 'http://localhost:8080/pacientes';
 
 const PacienteService = {
 
+  create: async (paciente: any) => {
+    const response = await axios.post(BASE_URL, paciente);
+    return response.data;
+  },
+
   getAll: async () => {
     const response = await axios.get(BASE_URL);
     return response.data;
